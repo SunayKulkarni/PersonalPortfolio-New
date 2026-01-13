66e59a0cc6e8f5cfe29f870e1bb292e1507784fb
Sunay Kulkarni <sunaykulkarni17@gmail.com>
2026-01-13 23:18:14 +0530

    tweaks in about section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -25,26 +25,6 @@ const experience = [
 
 export default function Experience() {
   return (
-    <div className="text-white mb-16">
-      <h1 className="text-2xl font-bold text-white">
-        <span className="text-orange-300">&gt;</span> Technical Activities
-      </h1>
-      <div className="mt-10">
-        {experience.map((exp) => (
-          <div key={exp.title} className="mt-10 group">
-            <a
-              href={exp.link}
-              className="text-xl font-bold transition-all duration-300 ease-in-out group-hover:text-green-300"
-            >
-              {exp.title}
-            </a>
-            <p className="text-gray-500 text-xs mt-2">
-              {exp.position} | {exp.date}
-            </p>
-            <div className="text-gray-400 mt-5 text-sm">{exp.description}</div>
-          </div>
-        ))}
-      </div>
-    </div>
+   <> </>
   );
 }
